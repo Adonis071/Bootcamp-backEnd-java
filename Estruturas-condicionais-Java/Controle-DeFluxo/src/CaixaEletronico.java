e19cfd831aca8335b4ca7376ea7e72f8d7a36c65
Adonis071 <adonisruis07@gmail.com>
2024-05-17 01:40:36 -0300

    commit controle de Fluxo

---
+++ Estruturas-condicionais-Java/Controle-DeFluxo/src/CaixaEletronico.java
@@ -0,0 +1,14 @@
+public class CaixaEletronico {
+    public static void main(String[] args) throws Exception {
+        Double saldo = 500.9;
+        Double valor = 90.9;
+
+        if (saldo > valor) {
+            saldo = valor - saldo;
+            System.out.println("Saque efetuado com sucesso");
+        } else {
+            System.out.println(saldo);
+        }
+
+    }
+}
